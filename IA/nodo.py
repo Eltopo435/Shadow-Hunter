f91b306a8c3961158181f54cb86d0e43388cf53d
Sergio gabriel <sergiogabrielperez06@gmail.com>
2025-03-11 22:02:06 +0000

    clase necesaria para el arbol de comportamiento

---
+++ IA/nodo.py
@@ -0,0 +1,52 @@
+# Sergio Gabriel Pérez A.
+# 23-EISN-2-028
+
+class Nodo:
+    def __init__(self, hijos=None):
+        self.hijos = hijos if hijos is not None else []
+
+    def agregar_hijo(self, hijo):
+        self.hijos.append(hijo)
+
+    def ejecutar(self):
+        pass
+
+
+class Selector(Nodo):
+    def ejecutar(self):
+        for hijo in self.hijos:
+            if hijo.ejecutar():
+                return True
+        return False
+
+
+class Secuencia(Nodo):
+    def ejecutar(self):
+        for hijo in self.hijos:
+            if not hijo.ejecutar():
+                return False
+        return True
+
+
+class Accion(Nodo):
+    def __init__(self, accion):
+        super().__init__()
+        self.accion = accion
+
+    def ejecutar(self):
+        return self.accion()
+
+
+class Timer(Nodo):
+    def __init__(self, tiempo):
+        super().__init__()
+        self.tiempo = tiempo
+        self.tiempo_restante = tiempo
+
+    def ejecutar(self):
+        if self.tiempo_restante > 0:
+            self.tiempo_restante -= 1
+            return False
+        else:
+            self.tiempo_restante = self.tiempo
+            return True if not self.hijos else self.hijos[0].ejecutar()
